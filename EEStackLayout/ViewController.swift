4d690c201deafa1b6b09312a7fe43398d910ce72
Efekan Egeli <efekan.egeli@trendyol.com>
2018-06-05 13:30:10 +0300

    Initial Commit

---
+++ EEStackLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EEStackLayout
+//
+//  Created by Efekan Egeli on 5.06.2018.
+//  Copyright © 2018 Efekan Egeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
