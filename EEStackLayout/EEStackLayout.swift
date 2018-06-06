3334964007edc0fab60ea0d9f96f003681d7a3e9
Efekan Egeli <efekan.egeli@trendyol.com>
2018-06-06 14:49:37 +0300

    Initial Commit

---
+++ EEStackLayout/EEStackLayout.swift
@@ -0,0 +1,102 @@
+//
+//  EEStackLayout.swift
+//  EEStackLayout
+//
+//  Created by Efekan Egeli on 5.06.2018.
+//  Copyright © 2018 Efekan Egeli. All rights reserved.
+//
+
+import UIKit
+
+open class EEStackLayout: UIStackView {
+    
+    // MARK: Private Logical Variables
+    private var privateTotalSubviewWidth = CGFloat(0)
+    private var privateRowView = UIView()
+    private var privatePreviousSubview = UIView()
+    private var privateTotalSubviewWidthWithSpacings = CGFloat(0)
+    
+    // MARK: Private UI Related Variables
+    private var privateRowHeight = CGFloat(0)
+    private var privateSubviewHeight = CGFloat(0)
+    private var privateSpacingBetweenSubviews = CGFloat(0)
+    private var privateLeftPadding = CGFloat(0)
+    private var privateRightPadding = CGFloat(0)
+    
+    // MARK: Initializers
+    public init(frame: CGRect, rowHeight: CGFloat, subviewHeight: CGFloat, spacingBetweenSubviews: CGFloat, leftPadding: CGFloat, rightPadding: CGFloat, subviews: [UIView]) {
+        super.init(frame: frame)
+        setupMainStackView()
+        setupRowProperties(rowHeight: rowHeight, subviewHeight: subviewHeight, spacingBetweenSubviews: spacingBetweenSubviews, leftPadding: leftPadding, rightPadding: rightPadding)
+        setupLayout(subviews: subviews)
+    }
+    
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: Property Setup
+    private func setupMainStackView() {
+        self.alignment = .fill
+        self.distribution = .fill
+        self.spacing = 0
+        self.axis = .vertical
+        addNewRow()
+    }
+    
+    private func setupRowProperties(rowHeight: CGFloat, subviewHeight: CGFloat, spacingBetweenSubviews: CGFloat, leftPadding: CGFloat, rightPadding: CGFloat) {
+        privateRowHeight = rowHeight
+        privateSubviewHeight = subviewHeight
+        privateSpacingBetweenSubviews = spacingBetweenSubviews
+        privateLeftPadding = leftPadding
+        privateRightPadding = rightPadding
+    }
+    
+    // MARK: Layout Setup
+    private func setupLayout(subviews: [UIView]) {
+        for subview in subviews {
+            if doesSubviewFitInRow(subview: subview) {
+                addSubviewToRow(subview: subview)
+            } else {
+                addNewRow()
+                addSubviewToRow(subview: subview)
+            }
+        }
+    }
+    
+    // MARK: Row Operations
+    private func doesSubviewFitInRow(subview: UIView) -> Bool {
+        privateTotalSubviewWidthWithSpacings = CGFloat(privateRowView.subviews.count - 1) * privateSpacingBetweenSubviews + privateTotalSubviewWidth
+        let totalPadding = privateLeftPadding + privateRightPadding
+        if privateTotalSubviewWidthWithSpacings > 0 &&
+            self.frame.size.width - (privateTotalSubviewWidthWithSpacings + totalPadding) < subview.frame.size.width + privateSpacingBetweenSubviews {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    private func addSubviewToRow(subview: UIView) {
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        privateRowView.addSubview(subview)
+        privateTotalSubviewWidth += subview.frame.size.width
+        if privateRowView.subviews.count == 1 {
+            subview.leadingAnchor.constraint(equalTo: privateRowView.leadingAnchor, constant: privateLeftPadding).isActive = true
+        } else {
+            subview.leadingAnchor.constraint(equalTo: privatePreviousSubview.trailingAnchor, constant: privateSpacingBetweenSubviews).isActive = true
+        }
+        subview.heightAnchor.constraint(equalToConstant: privateSubviewHeight).isActive = true
+        subview.widthAnchor.constraint(equalToConstant: subview.frame.size.width).isActive = true
+        subview.centerYAnchor.constraint(equalTo: privateRowView.centerYAnchor).isActive = true
+        privatePreviousSubview = subview
+    }
+    
+    private func addNewRow() {
+        privateTotalSubviewWidth = 0
+        privateRowView = UIView()
+        privateRowView.backgroundColor = .brown
+        privateRowView.heightAnchor.constraint(equalToConstant: privateRowHeight).isActive = true
+        self.addArrangedSubview(privateRowView)
+        self.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: self.frame.size.width, height: self.frame.size.height + CGFloat(privateRowHeight))
+    }
+}
--- EEStackLayout/ViewController.swift
+++ EEStackLayout/ViewController.swift
@@ -13,6 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = .orange
+        
+        var viewArray = [UIView]()
+        
+        for _ in 1...25 {
+            let view1 = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 42))
+            view1.backgroundColor = .green
+            
+            let view2 = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 42))
+            view2.backgroundColor = .blue
+            
+            let view3 = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 42))
+            view3.backgroundColor = .yellow
+            
+            let view4 = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 42))
+            view4.backgroundColor = .black
+            
+            viewArray.append(view1)
+            viewArray.append(view2)
+            viewArray.append(view3)
+            viewArray.append(view4)
+        }
+        
+        
+        let stackLayout = EEStackLayout(frame: CGRect(x: 0, y: 0, width: 380, height: 0),
+                                        rowHeight: CGFloat(42),
+                                        subviewHeight: CGFloat(20),
+                                        spacingBetweenSubviews: CGFloat(15),
+                                        leftPadding: CGFloat(15),
+                                        rightPadding: CGFloat(15),
+                                        subviews:viewArray)
+        self.view.addSubview(stackLayout)
     }
 
     override func didReceiveMemoryWarning() {
