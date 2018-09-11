b674d2a487feef7cb8913bccf6fb174453cced0e
Efekan Egeli <efekan.egeli@trendyol.com>
2018-09-11 11:08:51 +0300

    Add tests

--- EEStackLayout/Tests.swift
+++ EEStackLayout/Tests.swift
@@ -88,16 +88,4 @@ class Tests: XCTestCase {
         XCTAssertTrue(stackLayout.subviews.count == 1)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
